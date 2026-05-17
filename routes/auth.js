899950e6dd3637e53d1611c2d4748a62f15cae1a
Samruddhi Magdum <samruddhimagdum08@gmail.com>
2026-05-17 22:25:09 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('../controllers/authController');
-const { redirectIfAuthenticated } = require('../middleware/auth');
 
-// Pages
-router.get('/signup', redirectIfAuthenticated, authController.showSignup);
-router.get('/signin', redirectIfAuthenticated, authController.showSignin);
-
-// Auth APIs
-router.post('/signup', authController.signup);
+router.get('/signin', authController.showSignin);
 router.post('/signin', authController.signin);
+
+router.get('/signup', authController.showSignup);
+router.post('/signup', authController.signup);
+
 router.post('/logout', authController.logout);
 
 module.exports = router;
