4c894a548bbb1271180d26259e9820daac4ca684
Samruddhi Magdum <samruddhimagdum08@gmail.com>
2026-05-18 15:51:32 +0530

    Update cookie settings for token in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -128,12 +128,12 @@ const signIn = async (req, res) => {
     );
 
     // 🍪 SET COOKIE
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,   // IMPORTANT for Vercel
-      sameSite: "lax",
-      maxAge: 24 * 60 * 60 * 1000
-    });
+    res.cookie('token', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'lax',
+    maxAge: 24 * 60 * 60 * 1000
+  });
 
     return res.status(200).json({
       success: true,
